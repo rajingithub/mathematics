3d92841f693de59b959613a1010ee7371eb13f38
Mahender Reddy Borereddy <mahender@navyatech.in>
2023-08-22 16:35:15 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='mathematics',
+    version='0.0.1',
+    author='Mahender',
+    author_email='',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mike-huls/toolbox',
+    license='MIT',
+    packages=['mathematics'],
+    install_requires=['requests'],
+)
